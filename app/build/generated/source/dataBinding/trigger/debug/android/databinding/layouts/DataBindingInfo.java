d3a7214a34ee3824ef564a56ddab463e679d871d
unknown <darksautourus@hotmail.com>
2018-08-15 19:21:17 -0400

    first commit

---
+++ app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="6c085459-6a3d-42d8-8ab3-519da3f32648")
+public class DataBindingInfo {}
